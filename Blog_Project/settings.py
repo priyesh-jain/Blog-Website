22d206249463d221383a56f5788e8aff2b00ed77
priyesh-jain <priyeshjain360@gmail.com>
2022-04-25 08:24:12 +0530

    add origin name in setting.py

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z^)1&ztvaoyo625a75nbsiwtfa)o(+be&0hwx#hd^=cvq5316=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1',blogwebappli.herokuapp.com]
 
 
 # Application definition
